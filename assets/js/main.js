78810153a9ef042f9cf8fb8859f7c1e4e424186d
Bluecity Cab <rahuljoriyaofficial@gmail.com>
2023-05-24 17:22:43 +0530

    hi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,22 @@
-// FOR GLITCH HACKER EFFECT
+// FADE ANIMATION
+function fadeInOnScroll() {
+   const elements = document.querySelectorAll('.fade');
+   elements.forEach((element) => {
+     const rect = element.getBoundingClientRect();
+     const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+     if (rect.top <= windowHeight && rect.bottom >= 0) {
+       element.classList.add('visible');
+     } else {
+       element.classList.remove('visible');
+     }
+   });
+ }
+ 
+ document.addEventListener('DOMContentLoaded', fadeInOnScroll);
+ window.addEventListener('scroll', fadeInOnScroll);
+ 
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let intervals = [];
-
-const decoders = document.querySelectorAll(".decoder");
-
-for (const decoder of decoders) {
-  const originalText = decoder.innerText;
-  let interval = null;
-
-  decoder.addEventListener("mouseover", () => {
-    let iteration = 0;
-
-    clearInterval(interval);
-
-    interval = setInterval(() => {
-      decoder.innerText = originalText
-        .split("")
-        .map((letter, index) => {
-          if (index < iteration) {
-            return originalText[index];
-          }
-
-          return letters[Math.floor(Math.random() * 26)];
-        })
-        .join("");
-
-      if (iteration >= originalText.length) {
-        clearInterval(interval);
-      }
-
-      iteration += 1 / 4;
-    }, 30);
-  });
-
-  decoder.addEventListener("mouseout", () => {
-    clearInterval(interval);
-    decoder.innerText = originalText;
-  });
-
-  intervals.push(interval);
-}
 
 // SCROLL
 
