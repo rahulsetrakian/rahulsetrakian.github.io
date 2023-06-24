087e6a41303e96f773021fb9ea4802f0a451d133
rahulsetrakian <rahuljoriyaofficial@gmail.com>
2023-06-24 09:52:50 +0530

    next

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,22 +1,50 @@
-// Get the elements with the necessary classes
-const itemDataSpan = document.querySelector('.item-data');
-const itemText = document.querySelector('.item-text');
+  // Function to count words on the page
+  function countWords() {
+    var text = document.body.innerText || document.body.textContent;
+    var words = text.trim().split(/\s+/);
+    return words.length;
+  }
 
-// Function to calculate reading time in minutes
-function calculateReadingTime() {
-  // Assuming an average reading speed of 200 words per minute
-  const wordsPerMinute = 200;
+  // Function to calculate reading time
+  function calculateReadingTime() {
+    var wordsPerMinute = 200; // Adjust this value as desired
+    var wordCount = countWords();
+    var readingTime = Math.ceil(wordCount / wordsPerMinute);
+
+    return readingTime;
+  }
+
+  // Update the reading time in the HTML
+  var timeDataElement = document.getElementById("timeData");
+  if (timeDataElement) {
+    var readingTime = calculateReadingTime();
+    timeDataElement.textContent = readingTime;
+  }
+
+
+
   
-  // Get the text content and calculate the number of words
-  const textContent = itemText.textContent.trim();
-  const wordCount = textContent.split(' ').length;
-  
-  // Calculate the reading time in minutes
-  const readingTime = Math.ceil(wordCount / wordsPerMinute);
-  
-  // Fill the reading time into the span element
-  itemDataSpan.textContent = readingTime;
+  // Generate a random starting number between 1 and 1000
+const startingNumber = Math.floor(Math.random() * 1000) + 1;
+
+// Variable to keep track of the current number
+let currentNumber = startingNumber;
+
+// Update the span element with the current number
+const viewDataElement = document.getElementById("viewData");
+viewDataElement.textContent = currentNumber;
+
+// Function to generate and display the next random number
+function generateNextRandomNumber() {
+  // Increment the current number by 1
+  currentNumber++;
+
+  // Update the span element with the new current number
+  viewDataElement.textContent = currentNumber;
+
+  // Optional: Log the current number to the console
+  console.log(currentNumber);
 }
 
-// Call the function to calculate and fill the reading time
-calculateReadingTime();
+// Example usage:
+generateNextRandomNumber(); // Generates and displays the next random number
