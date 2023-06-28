b62a8350a3307b162ecaec0c6b86ae3209bad35c
rahulsetrakian <rahuljoriyaofficial@gmail.com>
2023-06-28 12:28:57 +0530

    ugo

--- assets/js/article.js
+++ assets/js/article.js
@@ -24,35 +24,5 @@
 
 
   
-  // Generate a random starting number between 1 and 1000
-const startingNumber = Math.floor(Math.random() * 1000) + 1;
-
-// Variable to keep track of the current number
-let currentNumber = startingNumber;
-
-// Update the span element with the current number
-const viewDataElement = document.getElementById("viewData");
-viewDataElement.textContent = currentNumber;
-
-// Function to generate and display the next random number
-function generateNextRandomNumber() {
-  // Increment the current number by 1
-  currentNumber++;
-
-  // Update the span element with the new current number
-  viewDataElement.textContent = currentNumber;
-
-  // Optional: Log the current number to the console
-  console.log(currentNumber);
-}
-
-// Example usage:
-generateNextRandomNumber(); // Generates and displays the next random number
-
-
-
-
-
-
 
 
