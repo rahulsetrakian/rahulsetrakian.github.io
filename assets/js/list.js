03c94b1dfef49d86e1eee2ef6df4e7ffb582038f
rahulsetrakian <rahuljoriyaofficial@gmail.com>
2023-06-28 16:21:38 +0530

    next

---
+++ assets/js/list.js
@@ -0,0 +1,23 @@
+const blog_description = document.querySelectorAll('.blog-description');
+
+blog_description.forEach(paragraph => {
+  const content = paragraph.textContent;
+  const words = content.trim().split(' ');
+  if (words.length > 24) {
+    const truncatedWords = words.slice(0, 24);
+    const truncatedContent = truncatedWords.join(' ');
+    paragraph.textContent = truncatedContent + '...';
+  }
+});
+
+const blog_title = document.querySelectorAll('.blog-title');
+
+blog_title.forEach(paragraph => {
+  const content = paragraph.textContent;
+  const words = content.trim().split(' ');
+  if (words.length > 6) {
+    const truncatedWords = words.slice(0, 6);
+    const truncatedContent = truncatedWords.join(' ');
+    paragraph.textContent = truncatedContent + '...';
+  }
+});
