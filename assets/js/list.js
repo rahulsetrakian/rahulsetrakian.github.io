7e530d5ff40380b7f0317169f716af9334545fd7
rahulsetrakian <rahuljoriyaofficial@gmail.com>
2023-09-10 13:05:54 +0530

    next

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,3 +1,54 @@
+window.onload = function() {
+  const searchInput = document.getElementById("search-bar");
+  const iconElement = document.querySelector(".bx-search");
+
+  if (searchInput && iconElement) {
+    // Function to change the icon class with a transition
+    function changeIcon() {
+      // Add a CSS class that defines the transition
+      iconElement.classList.add("transition-class");
+
+      // Change the icon class
+      iconElement.classList.remove("bx-search");
+      iconElement.classList.add("bx-x");
+
+      // Remove the transition class after a delay (adjust duration as needed)
+      setTimeout(() => {
+        iconElement.classList.remove("transition-class");
+      }, 300); // 300 milliseconds, adjust as needed
+    }
+
+    // Function to revert the icon class with a transition
+    function revertIcon() {
+      // Add a CSS class that defines the transition
+      iconElement.classList.add("transition-class");
+
+      // Revert the icon class
+      iconElement.classList.remove("bx-x");
+      iconElement.classList.add("bx-search");
+
+      // Remove the transition class after a delay (adjust duration as needed)
+      setTimeout(() => {
+        iconElement.classList.remove("transition-class");
+      }, 300); // 300 milliseconds, adjust as needed
+    }
+
+    // Add an onclick event listener to the input to change the icon
+    searchInput.addEventListener("click", changeIcon);
+
+    // Add a blur event listener to the input to revert the icon
+    searchInput.addEventListener("blur", revertIcon);
+  } else {
+    console.error("Search input or icon element not found.");
+  }
+};
+
+
+
+
+
+
+
 const blog_description = document.querySelectorAll('.blog-description');
 
 blog_description.forEach(paragraph => {
