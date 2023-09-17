551cd5452ce87d26b95a1fa9d0b8daad4fe3e8b5
rahulsetrakian <rahuljoriyaofficial@gmail.com>
2023-09-17 09:58:12 +0530

    search done

--- assets/js/list.js
+++ assets/js/list.js
@@ -42,33 +42,3 @@ window.onload = function() {
     console.error("Search input or icon element not found.");
   }
 };
-
-
-
-
-
-
-
-const blog_description = document.querySelectorAll('.blog-description');
-
-blog_description.forEach(paragraph => {
-  const content = paragraph.textContent;
-  const words = content.trim().split(' ');
-  if (words.length > 24) {
-    const truncatedWords = words.slice(0, 24);
-    const truncatedContent = truncatedWords.join(' ');
-    paragraph.textContent = truncatedContent + '...';
-  }
-});
-
-const blog_title = document.querySelectorAll('.blog-url');
-
-blog_title.forEach(paragraph => {
-  const content = paragraph.textContent;
-  const words = content.trim().split(' ');
-  if (words.length > 6) {
-    const truncatedWords = words.slice(0, 6);
-    const truncatedContent = truncatedWords.join(' ');
-    paragraph.textContent = truncatedContent + '...';
-  }
-});
--- assets/js/main.js
+++ assets/js/main.js
@@ -165,4 +165,31 @@ function closePopup() {
 }
 
 
+
+
+
+const blog_description = document.querySelectorAll('.blog-description');
+
+blog_description.forEach(paragraph => {
+  const content = paragraph.textContent;
+  const words = content.trim().split(' ');
+  if (words.length > 24) {
+    const truncatedWords = words.slice(0, 24);
+    const truncatedContent = truncatedWords.join(' ');
+    paragraph.textContent = truncatedContent + '...';
+  }
+});
+
+const blog_title = document.querySelectorAll('.blog-url');
+
+blog_title.forEach(paragraph => {
+  const content = paragraph.textContent;
+  const words = content.trim().split(' ');
+  if (words.length > 6) {
+    const truncatedWords = words.slice(0, 6);
+    const truncatedContent = truncatedWords.join(' ');
+    paragraph.textContent = truncatedContent + '...';
+  }
+});
+
 // TEST CODE
